a5c0c4ad3215ea768c3a4b3545d7fa1513d2b2ca
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-04-19 16:47:15 -0500

    Tabla como componente

--- src/components/Usuarios/index.js
+++ src/components/Usuarios/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux';
 import Spinner from './../General/Spinner';
 import Error404 from '../General/Error404';
+import Tabla from './Tabla';
 
 import * as usuariosActions from './../../actions/usuariosActions';
 
@@ -20,43 +21,15 @@ class Usuarios extends Component {
       return <Error404 mensaje={ this.props.error } />;
     }
 
-    return (
-      <table className='tabla'>
-        <thead>
-           <tr>
-            <th>Nombre</th>
-            <th>Correo</th>
-            <th>Enlace</th>
-          </tr>
-        </thead>
-        <tbody>
-           { this.ponerFilas() }
-        </tbody>
-      </table>
-    )
+    return <Tabla />
   }
-
-  ponerFilas = () => (
-    this.props.usuarios.map((usuario) => (
-      <tr key={ usuario.id }>
-        <td>
-          { usuario.name }
-        </td>
-        <td>
-          { usuario.email }
-        </td>
-        <td>
-          { usuario.website }
-        </td>
-      </tr>
-    ))
-  )
 
   render() {
     // console.log(this.props.cargando);
     // console.log(this.props.error);
     return (
       <div>
+        <h1>Usuarios</h1>
         { this.ponerContenido() }
       </div>
     )
