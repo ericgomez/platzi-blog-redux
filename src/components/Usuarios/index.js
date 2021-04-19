e53dcf3d4f5311e1d447144c137fc6aab2606fa7
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-04-19 13:34:17 -0500

    Conexión a un componente

--- src/components/Usuarios/index.js
+++ src/components/Usuarios/index.js
@@ -1,25 +1,19 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import { connect } from 'react-redux';
 
 class Usuarios extends Component {
 
-  constructor() {
-    super();
-    this.state = {
-      usuarios: []
-    }
-  }
-
-  async componentDidMount(){
+  /* async componentDidMount(){
     const respuesta = await axios.get('https://jsonplaceholder.typicode.com/users');
     console.log('3. componentDidMount()');
     this.setState({
       usuarios: respuesta.data
     })
-  }
+  } */
 
   ponerFilas = () => (
-    this.state.usuarios.map((usuario) => (
+    this.props.usuarios.map((usuario) => (
       <tr key={ usuario.id }>
         <td>
           { usuario.name }
@@ -54,4 +48,8 @@ class Usuarios extends Component {
   }
 }
 
-export default Usuarios
+const mapStateToProps = (reducers) => {
+	return reducers.usuariosReducer;
+};
+
+export default connect(mapStateToProps, {/*Accion Creator*/})(Usuarios)
