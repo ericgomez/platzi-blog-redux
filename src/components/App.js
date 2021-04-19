4c5cda6548e1d2ea1244bbe416f3b2b45c9059c4
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-04-19 11:53:38 -0500

    Ciclo de vida de React e implementando Axios para peticiones Http

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 class App extends Component {
 
   constructor() {
     super();
     this.state = {
+      usuarios: []
+    }
+  }
+
+  async componentDidMount(){
+    const respuesta = await axios.get('https://jsonplaceholder.typicode.com/users');
+    console.log('Respuesta:', respuesta);
+    console.log('3. componentDidMount()');
+    this.setState({
       usuarios: [
         {
           nombre: 'Eric',
@@ -17,7 +27,7 @@ class App extends Component {
           enlace: 'prodeco.com'
         },
       ]
-    }
+    })
   }
 
   ponerFilas = () => (
@@ -37,6 +47,7 @@ class App extends Component {
   )
 
   render() {
+    console.log(this.state.usuarios);
     return (
       <div className='margen'>
         <table className='tabla'>
