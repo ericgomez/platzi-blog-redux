3fd41720b7eb0ca1af027f44851b42be4e88c2bc
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-04-20 10:14:44 -0500

    Evitar sobrescritura

--- src/components/Publicaciones/index.js
+++ src/components/Publicaciones/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Spinner from '../General/Spinner';
+import Error404 from '../General/Error404';
 
 import * as usuariosActions from './../../actions/usuariosActions';
 import * as publicacionesActions from './../../actions/publicacionesActions';
@@ -10,17 +12,52 @@ const { traerPorUsuario: publicacionesTraerPorUsuario } = publicacionesActions;
 class Publicaciones extends Component {
 
 	async componentDidMount() {
+		const {
+			usuariosTraerTodos,
+			match: { params: { key } },
+			publicacionesTraerPorUsuario
+		} = this.props;
+
 		if (!this.props.usuariosReducer.usuarios.length) {
-			await this.props.usuariosTraerTodos();
+			await usuariosTraerTodos();
 		}
-		this.props.publicacionesTraerPorUsuario(this.props.match.params.key);
+
+		if (this.props.usuariosReducer.error) {
+			return;
+		}
+
+		if (!('publicaciones_key' in this.props.usuariosReducer.usuarios[key])) {
+			await publicacionesTraerPorUsuario(key);
+		}
 	}
+
+	ponerNombre = () => {
+		const {
+			match: { params: { key } },
+			usuariosReducer
+		} = this.props;
+
+		if (usuariosReducer.error) {
+			return <Error404 mensaje={ usuariosReducer.error } />;
+		}
+		if (!usuariosReducer.usuarios.length || usuariosReducer.cargando) {
+			return <Spinner />
+		}
+    // Nombre del usuario
+		const nombre = usuariosReducer.usuarios[key].name;
+
+		return (
+			<h1>
+				Publicaciones de { nombre }
+			</h1>
+		);
+	};
 
 	render() {
 		console.log(this.props);
 		return (
 			<div>
-				<h1>Publicaciones de </h1>
+				{ this.ponerNombre() }
 				{ this.props.match.params.key }
 			</div>
 		);
