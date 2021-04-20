bdeabbd9397fbbb33c170fbb78c22f04c9ea44a3
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-04-20 11:40:48 -0500

    Llamadas asincronas dinámicas

--- src/components/Publicaciones/index.js
+++ src/components/Publicaciones/index.js
@@ -88,7 +88,8 @@ class Publicaciones extends Component {
 			<div 
 			className="pub_titulo"
 			key={publicacion.id}
-			onClick={ ()=> this.props.mostrarComentarios(pub_key, com_key) }>
+			onClick={ () => this.mostrarComentarios(pub_key, com_key, publicacion.comentarios) }
+			>
 				<h2>
 					{ publicacion.title}
 				</h2>
@@ -101,10 +102,11 @@ class Publicaciones extends Component {
 	);
 
 	mostrarComentarios = (pub_key, com_key, comentarios) => {
-
 		this.props.abrirCerrar(pub_key, com_key)
-		this.props.traerComentarios(pub_key, com_key);
 		
+		if (!comentarios.length) {
+			this.props.traerComentarios(pub_key, com_key);
+		}
 	};
 
 	render() {
